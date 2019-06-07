930a2c748ce57b84ddd4f604a88f812a61ab70d3
Manasseh Omachonu <manaraph225@gmail.com>
2019-06-07 12:52:41 +0100

    Progress with http-headers bug

--- my-react-app/src/pages/PostsManager.js
+++ my-react-app/src/pages/PostsManager.js
@@ -2,143 +2,143 @@ import React, { Component, Fragment } from 'react';
 import { withAuth } from '@okta/okta-react';
 import { withRouter, Route, Redirect, Link } from 'react-router-dom';
 import {
-    withStyles,
-    Typography,
-    Button,
-    IconButton,
-    Paper,
-    List,
-    ListItem,
-    ListItemText,
-    ListItemSecondaryAction,
-} from '@material-ui/core'
-import { Delete as DeleteIcon, Add as AddIcon } from '@material-ui/icons'
+  withStyles,
+  Typography,
+  Button,
+  IconButton,
+  Paper,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+} from '@material-ui/core';
+import { Delete as DeleteIcon, Add as AddIcon } from '@material-ui/icons';
 import moment from 'moment';
 import { find, orderBy } from 'lodash';
 import { compose } from 'recompose';
 
 import PostEditor from '../components/PostEditor';
-import { number } from 'prop-types';
 
 const styles = theme => ({
-    posts: {
-        marginTop: theme.spacing(2),
+  posts: {
+    marginTop: theme.spacing(2),
+  },
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing(3),
+    right: theme.spacing(3),
+    [theme.breakpoints.down('xs')]: {
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
     },
-    fab: {
-        position: 'absolute',
-        bottom: theme.spacing(3),
-        right: theme.spacing(3),
-        [theme.breakpoints.down('xs')]: {
-            bottom: theme.spacing(2),
-            right: theme.spacing(2),
-        },
-    },
-})
+  },
+});
 
 const API = process.env.REACT_APP_API || 'http://localhost:3001';
 
 class PostsManager extends Component {
-    state = {
-        loading: true,
-        posts: [],
-    };
+  state = {
+    loading: true,
+    posts: [],
+  };
 
-    componentDidMount() {
-        this.getPosts();
-    }
-    
-    async fetch(method, endpoint, body){
-        try {
-            const response = await fetch(`${API}${endpoint}`, {
-                method,
-                headers: {
-                    'content-type': 'application/json',
-                    accept: 'application/json',
-                    authorization: `Bearer ${await this.props.auth.getAccessToken()}`,
-                },
-                body: body && JSON.stringify(body),
-            });
-            return await response.json();
-        } catch (error) {
-            console.log(error);
-                        
-        }
-    }
-    
-    async getPosts() {
-        this.setState({ loading: false, posts: await this.fetch('get', '/posts')});
-    }
+  componentDidMount() {
+    this.getPosts();
+  }
 
-    savePost = async (post) => {
-        if (post.id) {
-            await this.fetch('put', `/posts/${post.id}`, post);
-        } else {
-            await this.fetch('post', '/posts', post);
-        }
+  async fetch(method, endpoint, body) {
+    try {
+      const response = await fetch(`${API}${endpoint}`, {
+        method,
+        body: body && JSON.stringify(body),
+        headers: {
+          'content-type': 'application/json',
+          accept: 'application/json',
+          authorization: `Bearer ${await this.props.auth.getAccessToken()}`,
+        },
+      });
+      return await response.json();
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-        this.props.history.goBack();
-        this.getPosts();
+  async getPosts() {
+    this.setState({ loading: false, posts: await this.fetch('get', '/posts') });
+  }
+
+  savePost = async (post) => {
+    if (post.id) {
+      await this.fetch('put', `/posts/${post.id}`, post);
+    } else {
+      await this.fetch('post', '/posts', post);
     }
 
-    async deletePost(post) {
-        if (window.confirm(`Are you sure you want to delete "${post.title}"`)){
-            await this.fetch('delete', `/posts/${post.id}`);
-            this.getPosts();
-        }
+    this.props.history.goBack();
+    this.getPosts();
+  }
+
+  async deletePost(post) {
+    if (window.confirm(`Are you sure you want to delete "${post.title}"`)) {
+      await this.fetch('delete', `/posts/${post.id}`);
+      this.getPosts();
     }
+  }
 
-    renderPostEditor = ({ match:  { params: { id }}}) => {
-        if (this.state.loading) return null;
-        const post = find(this.state.posts, { id: Number(id) });
+  renderPostEditor = ({ match: { params: { id } } }) => {
+    if (this.state.loading) return null;
+    const post = find(this.state.posts, { id: Number(id) });
 
-        if (!post && id !== 'new') return <Redirect to="/posts" />;
+    if (!post && id !== 'new') return <Redirect to="/posts" />;
 
-        return <PostEditor post={post} onSave={this.savePost} />;
-    }
+    return <PostEditor post={post} onSave={this.savePost} />;
+  };
 
-    render() {
-        const { classes } = this.props;
+  render() {
+    const { classes } = this.props;
 
-        return (
-            <Fragment>
-                <Typography>Posts Manager</Typography>
-                {this.state.posts.length > 0 ? (
-                    <Paper elevation={1} className={classes.posts}>
-                        <List>
-                            {orderBy(this.state.posts, ['updatedAt', 'title'], ['desc', 'asc']).map(post => (
-                                <ListItem key={post.id} button component={Link} to={`/posts/${post.id}`}>
-                                    <ListItemText
-                                    primary={post.title}
-                                    secondary={post.updatedAt && `updated ${moment(post.updatedAt).fromNow()}`}
-                                    />
-                                    <ListItemSecondaryAction>
-                                        <IconButton onClick={() => this.deletePost(post) } color="inherit">
-                                            <DeleteIcon />
-                                        </IconButton>
-                                    </ListItemSecondaryAction>
-                                </ListItem>
-                            ))}
-                        </List>
-                    </Paper>
-                ) : (
-                    !this.state.loading && <Typography variant="subtitle1">No posts to display</Typography>
-                )}
-                <Button
-                    // variant="fab"
-                    color="secondary"
-                    aria-label="add"
-                    className={classes.fab}
-                    component={Link}
-                    to="/posts/new"
-                >   
-                        <AddIcon />
-                </Button>
-                <Route exact path="/posts/:id" render={this.renderPostEditor} />
-            </Fragment>
-        );
-    }
+    return (
+      <Fragment>
+        <Typography>Posts Manager</Typography>
+        {this.state.posts.length > 0 ? (
+          <Paper elevation={1} className={classes.posts}>
+            <List>
+              {orderBy(this.state.posts, ['updatedAt', 'title'], ['desc', 'asc']).map(post => (
+                <ListItem key={post.id} button component={Link} to={`/posts/${post.id}`}>
+                  <ListItemText
+                    primary={post.title}
+                    secondary={post.updatedAt && `Updated ${moment(post.updatedAt).fromNow()}`}
+                  />
+                  <ListItemSecondaryAction>
+                    <IconButton onClick={() => this.deletePost(post)} color="inherit">
+                      <DeleteIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              ))}
+            </List>
+          </Paper>
+        ) : (
+          !this.state.loading && <Typography >No posts to display</Typography>
+        )}
+        <Button
+        //   variant="fab"
+          color="secondary"
+          aria-label="add"
+          className={classes.fab}
+          component={Link}
+          to="/posts/new"
+        >
+          <AddIcon />
+        </Button>
+        <Route exact path="/posts/:id" render={this.renderPostEditor} />
+      </Fragment>
+    );
+  }
 }
 
 export default compose(
-    withAuth, withRouter, withStyles(styles),
-)(PostsManager)
+  withAuth,
+  withRouter,
+  withStyles(styles),
+)(PostsManager);
