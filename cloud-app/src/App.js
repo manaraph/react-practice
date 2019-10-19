f8ce72ad9dee5066e9b579e67a47cbd032bc9c31
Manasseh <manaraph225@gmail>
2019-10-19 02:55:39 +0100

    Init react app and create docker configuration

---
+++ cloud-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hey its <abbr title="Manasseh Omachonu">m4</abbr>, we just playing around with some react app.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
